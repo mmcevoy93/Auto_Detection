a6d16064687e34de869be63787bac8d86e12a8e0
mmcevoy93 <mmcevoy@ualberta.ca>
2018-02-16 20:40:16 -0700

    Update. Need to consider different lighting

--- max_version.py
+++ max_version.py
@@ -6,15 +6,28 @@ import numpy as np
 import cv2
 import sys
 
+
+def set_low_up(Lighting):
+
+    if Lighting == "Dark":
+        lower = np.array([100, 30, 50])
+        upper = np.array([130, 255, 255])
+    if Lighting == "Bright":
+        lower = np.array([100, 0, 0])
+        upper = np.array([200, 20, 255])
+
+
+    return lower, upper
+
+
 if __name__ == "__main__":
 
-    lower_blue = np.array([100, 50, 50])
-    upper_blue = np.array([110, 255, 255])
+
     file_name = sys.argv[1]
     frame = cv2.imread(file_name)
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    frame_mask = cv2.inRange(frame_HSV, lower_blue, upper_blue)
+    lower, upper = set_low_up("Dark")
+    frame_mask = cv2.inRange(frame_HSV, lower, upper)
 
     frame_applied = cv2.bitwise_and(frame, frame, mask=frame_mask)
 
@@ -24,7 +37,7 @@ if __name__ == "__main__":
     # display our input and output images to compare
     cv2.imshow('Input Image', frame)
     cv2.imshow('Output Image', frame_applied)
-    cv2.moveWindow('Input image', 1, 0)
+    cv2.moveWindow('Input image', 0, 0)
     cv2.moveWindow('Output Image', 650, 0)
 
     cv2.waitKey(0)
